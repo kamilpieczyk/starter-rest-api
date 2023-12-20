274c1af3bdda5807d51e5b648ced0acda7cf2803
KamilAtSherwen <149412684+KamilAtSherwen@users.noreply.github.com>
2023-12-20 13:14:25 +0000

    add host

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Router } from 'express'
 import cors from 'cors'
 import { config } from 'dotenv'
 import { createServer } from 'http'
@@ -10,8 +10,9 @@ const app: Application = express()
 app.use(cors({ origin: '*', optionsSuccessStatus: 200 }))
 const server = createServer(app)
 
-server.listen(process.env.port, () => {
+server.listen({ port: process.env.port, host: '0.0.0.0' }, () => {
   console.log(`Server running on port ${process.env.port}`)
 })
+
 
 ws(server)
